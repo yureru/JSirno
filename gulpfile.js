98ed926818058ffeb8456178a7cb8b5fdcbdebe0
roli <roli@cock.li>
2017-05-26 22:18:06 -0500

    First commit. Adds .gitignore, .babelrc, and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+// Gulp dependencies go here
+gulp.task('default', function() {
+  // Node source
+  gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+    // browser source
+    gulp.src("public/es6/**/*.js")
+      .pipe(babel())
+      .pipe(gulp.dest("public/dist"));
+});
